5e487140a8087721de983de6a8917d0529c352da
ADRIANA <ADRIANA@DESKTOP-GPSE2AB>
2021-11-16 16:44:06 -0500

    Primera version Vista

--- src/pkgfinal/Final.java
+++ src/pkgfinal/Final.java
@@ -12,6 +12,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import Logica.ConexionBD;
+import Logica.MedicamentosSolidos;
+import java.util.List;
 
 /**
  *
@@ -23,39 +26,13 @@ public class Final {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String sql;
-        String driver = "com.mysql.cj.jdbc.Driver";
-        String URL_bd = "jdbc:mysql://localhost:3306/prodactuz";
-        String usuario = "root";
-        String clave = "";
-        Connection con = null;
-        Statement comando = null;
-        ResultSet registros = null;
-        int filas_afectadas;
-
-        try {
-            Class.forName(driver);
-            con = DriverManager.getConnection(URL_bd, usuario, clave);
-            if (con != null) {
-                System.out.println("Conexion Exitosa");
-                comando = con.createStatement();
-                sql = "select nombre from tbl_administradores";
-                registros = comando.executeQuery(sql);
-                while (registros.next()) {
-                    System.out.println("Nombre:" + registros.getString("nombre"));
-                }
-                sql = "INSERT INTO tbl_administradores(nombre) VALUES ('Arnulfo');";
-                filas_afectadas = comando.executeUpdate(sql);
-                System.out.println("Se afectaron " + filas_afectadas + " registros");
-                con.close();
-            } else {
-                System.out.println("Conexion No Exitosa");
-            }
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Final.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            Logger.getLogger(Final.class.getName()).log(Level.SEVERE, null, ex);
+        ConexionBD conexion = new ConexionBD();
+        MedicamentosSolidos solidos1 = new MedicamentosSolidos();
+        List<MedicamentosSolidos> lista = solidos1.listarSolidos();
+        for (MedicamentosSolidos sol : lista){
+            System.out.println(sol);
         }
+        
     }
 // TODO code application logic here
 }
